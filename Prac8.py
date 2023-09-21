2bec7d1b31837f82967dbfe2aed03bc22096b65e
yagna <yagnapatelhirenk@gmail.com>
2023-09-21 10:20:29 +0530

    PRAC8

--- Prac8.py
+++ Prac8.py
@@ -3,7 +3,7 @@
 def lcs(X, Y, m, n):
 
 	elements = []
-	L = [[None]*(n+1) for i in range(m+1)]
+	L = [[0]*(n+1) for i in range(m+1)]
 	print(L)
 	for i in range(m+1):
 		for j in range(n+1):
@@ -11,17 +11,38 @@ def lcs(X, Y, m, n):
 				L[i][j] = 0
 			elif X[i-1] == Y[j-1]:
 				L[i][j] = L[i-1][j-1]+1
-				elements.append(L[i][j])
+				elements.append([i,j])
 			else:
 				L[i][j] = max(L[i-1][j], L[i][j-1])
 
+	index = L[m][n]
+	Lcs_char = [""] * (index+1)
+	Lcs_char[index] = ''
+
+	p=m
+	q=n
 	
-	return elements
+	while p>0 and q>0:
+		if X[p-1] == Y[q-1]:
+			Lcs_char[index-1] = X[p-1]
+			p -= 1
+			q -=1
+			index -= 1
+		elif L[p-1][q] > L[p][q-1]:
+			p -=1
+		else : 
+			q -=1
+	
+	print("\nX : " + X + "\nY : " + Y)
+	print("\nLength of LCS is", L[m][n])
+	print("\nLCS : " + "".join(Lcs_char))
+	print()
+
+	return L[m][n]
 
 
 S1 = "AGGTAB"
 S2 = "GXTXAYB"
 m = len(S1)
 n = len(S2)
-print("Length of LCS is", lcs(S1, S2, m, n))
-
+lcs(S1,S2,m,n)
