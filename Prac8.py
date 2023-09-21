960aa1e5228aca23603ddaa3c3910d68e9a07bbf
yagna <yagnapatelhirenk@gmail.com>
2023-09-21 11:15:15 +0530

    Prac9

--- Prac8.py
+++ Prac8.py
@@ -4,7 +4,6 @@ def lcs(X, Y, m, n):
 
 	elements = []
 	L = [[0]*(n+1) for i in range(m+1)]
-	print(L)
 	for i in range(m+1):
 		for j in range(n+1):
 			if i == 0 or j == 0:
---
+++ Prac9.py
@@ -0,0 +1,19 @@
+def coinChange(coins,amount):
+    dp = [float('inf')]* (amount+1)
+    dp[0] = 0
+    coin_used = [[] for _ in range(amount+1)]
+    for coin in coins:
+        for i in range(coin,amount+1):
+            if dp[i] > (dp[i-coin]+1):
+                dp[i] = dp[i-coin] + 1
+                coin_used[i] = coin_used[i-coin] + [coin]
+    if dp[amount] == float('inf'):
+        return -1,[]
+    else :
+        return dp[amount], coin_used[amount]
+
+coins = [1,4,6]
+amount = 9
+min_coins, coins_used = coinChange(coins,amount)
+print('Minimum amount of coins needed :',min_coins)
+print('Coins used : ',coins_used)
