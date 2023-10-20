8bbc9510f8d751588687f9d03c3a64f72b233a9d
yagna <yagnapatelhirenk@gmail.com>
2023-10-20 11:52:50 +0530

    prac11

--- Prac11.py
+++ Prac11.py
@@ -9,12 +9,10 @@ def knapSack(max_W, wt, profits):
 				K[i][w] = max(profits[i-1]+ K[i-1][w-wt[i-1]],K[i-1][w])
 			else:
 				K[i][w] = K[i-1][w]
-	for j in K:
-		print(j)
 	return K[len(profits)][max_W]
 
 
-profits = [3,4,5,6]
-weights = [2,3,4,5]
-max_W = 5
+profits = [280,100,120,120]
+weights = [40,10,20,24]
+max_W = 60
 print(knapSack(max_W, weights, profits))
