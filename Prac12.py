715f6011cef75c6091d94aae579b48a1bb4e437e
yagna <yagnapatelhirenk@gmail.com>
2023-10-20 11:52:57 +0530

    prac12

--- Prac12.py
+++ Prac12.py
@@ -4,10 +4,9 @@ class Item:
 		self.profit = profit
 		self.weight = weight
 
-# Main greedy function to solve problem
+
 def fractionalKnapsack(W, arr):
 	arr.sort(key=lambda x: (x.profit/x.weight), reverse=True)
-	# Result(value in Knapsack)
 	profits = []
 	weights = []
 	Max_profit = 0.0
@@ -17,16 +16,10 @@ def fractionalKnapsack(W, arr):
 		weights.append(a.weight)
 	for item in arr:
 
-		# If adding Item won't overflow, 
-		# add it completely
 		if item.weight <= W:
 			W -= item.weight
 			Max_profit += item.profit
 			ratio.append(1)
-		# 2,3,4 W = 3 
-
-		# If we can't add current Item, 
-		# add fractional part of it
 		else:
 			Max_profit += item.profit * W / item.weight
 			ratio.append(round(W/item.weight,2))
@@ -45,6 +38,5 @@ weights = list(map(int,input('Enter the weights : ').split(' ')))
 Cap = int(input("Enter max weight :"))
 
 PW = [Item(profit, weight) for profit,weight in zip(profits,weights)]
-# Function call
 
 fractionalKnapsack(Cap, PW)
